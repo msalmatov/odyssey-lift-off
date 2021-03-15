f8ebfaa616244a838fcf424ad866184bf3a3d88f
mroot <almatov.ms@gmail.com>
2021-03-15 15:43:24 +0500

    client: wrap query result with QueryResult component

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout } from "../components";
 import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 const TRACKS = gql`
   query ExampleQuery {
@@ -27,15 +28,13 @@ const TRACKS = gql`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-
-  if (error) return `ERROR: ${error.message}`;
-
   return (
     <Layout grid>
-      {data?.tracksForHome?.map((track) => (
-        <TrackCard key={track.id} track={track} />
-      ))}
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
     </Layout>
   );
 };
